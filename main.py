22028107dadce732d1c8d3eb9714d8c154cbb7d1
buhshtabd <116212638+B-den@users.noreply.github.com>
2022-11-08 22:03:49 +0300

    save_file

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from db.models.person import Person
 from db.models.password import Password
 from db.funcs import *
 
+
 app = FastAPI()
 
 @app.post('/auth')
@@ -23,12 +24,17 @@ async def auth(data=Body(), type: str = Query(default="old")):
     if type == "new":
         return await new_user(data)
 
+async def save_file(in_file: UploadFile, out_file_path: str):
+    async with aiofiles.open(out_file_path, 'wb') as out_file:
+        while content := await in_file.read(1024):  # async read chunk
+            await out_file.write(content)  # async write chunk
+
 @app.post('/upload_file')
 async def process_file(file: UploadFile):
-    async with aiofiles.open('0.txt', "a") as out:
-        content = await file.read()
-        print(content)
-        await out.write(str(content))
+    #async with aiofiles.open('0.png', "wb") as out:
+    #    content = await file.read()
+    #    await out.write(content)
+    #await save_file(file, '1.png')
     await file.close()
     return {"filename" : file.filename }
 
